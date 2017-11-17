f0bfea99e68cd953c8c9eb36daa2537cd0b57a71
Oleksandr Kovalenko <oleksandr.kovalenko@growbots.com>
2017-11-17 17:36:14 +0100

    Add more nonlinear models.

--- test/test_train_models.py
+++ test/test_train_models.py
@@ -13,8 +13,36 @@ class TestDifferentModles(unittest.TestCase):
         ttr.check_model('SGD', SGDRegressor())
 
 
+    def test_lasso(self):
+        from sklearn.linear_model import Lasso
+        ttr.check_model('Lasso', Lasso())
+
+
+    def test_ridge(self):
+        from sklearn.linear_model import Ridge 
+        ttr.check_model('Ridge', Ridge(alpha=0.1))
+
+
+    def test_elastic(self):
+        from sklearn.linear_model import ElasticNet 
+        ttr.check_model('ElasticNet', ElasticNet(alpha=1))
+
+
+    def test_svr(self):
+        from sklearn.svm import SVR 
+        # TODO: Try to search in differnt kernel functions
+        #       rbf, polynomial
+        ttr.check_model('SVR', SVR(kernel='poly', degree=4))
+
+
+
+    # TODO: Add separate tests for ensemble regressors
+    #
+    
+    @unittest.skip('')
     def test_random_forest(self):
         from sklearn.ensemble import RandomForestRegressor
         ttr.check_model('Random Forest', RandomForestRegressor())
+
 
 
