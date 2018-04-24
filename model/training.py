543eb1344f24d0538d435efd7b3bbe6eda79e0c6
Iohan Petrov <petrov.knu@gmail.com>
2018-04-24 14:26:42 +0100

    Remove automatic scaler from the trainer

--- model/training.py
+++ model/training.py
@@ -1,14 +1,7 @@
 from data import DataHandler
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-
 from matplotlib import pyplot as plt
-import seaborn
-
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 # Avoid sklearn version mismatch
 try:
@@ -21,23 +14,16 @@ class Trainer(object):
     def __init__(self):
         super(Trainer, self).__init__()
 
-    # NB: Now it's easy to extend the training proces 
+    # NB: Now it's easy to extend the training proces
     #     by taking adwantage of iheritance. It can be useful in future
     #
-
-    @classmethod
-    def pipeline(klass, regressor):
-        return make_pipeline(StandardScaler(), regressor)
-
 
     @classmethod
     def check_model(klass, name, regressor):
         (X, y), (X_test, y_test) = DataHandler.load_train_test()
 
-        estimator = klass.pipeline(regressor)
-        estimator.fit(X, y)
-        return klass._check(estimator, name, X, y, X_test, y_test)
-
+        regressor.fit(X, y)
+        return klass._check(regressor, name, X, y, X_test, y_test)
 
     @classmethod
     def _check(klass, estimator, name, X, y, X_test, y_test):
@@ -52,33 +38,31 @@ class Trainer(object):
         print 'On test set MSE {0}'.format(mean_squared_error(predictions, y_test))
         print '======================================'
 
-   
-
         plt.figure()
         # plt.subplot(1, 2, 1)
         plt.grid(True)
-        plt.scatter(y, X, alpha = 0.5, color = 'red', label = 'training data')
-        plt.scatter(y_test, predictions, alpha = 0.5, color = 'blue', label = 'test data')
+        plt.scatter(y, X, alpha=0.5, color='red', label='training data')
+        plt.scatter(y_test, predictions, alpha=0.5,
+                    color='blue', label='test data')
         plt.legend()
         plt.title('Input/Output correlation for {0}'.format(name))
-        plt.axes().set_aspect('equal')
+        plt.axes().set_aspect('equal', 'datalim')
         plt.xlabel('true values')
         plt.ylabel('predicted values')
         plt.show()
         print 'On train set MSE {0}'.format(mean_squared_error(X, y))
         return mean_absolute_error(predictions, y_test)
 
-    @classmethod   
+    @classmethod
     def search(klass, name, regressor, parameters):
+        print "Tuning the parameters.\nAll available:"
+        for k in regressor.get_params().keys():
+            print k
         (X, y), (X_test, y_test) = DataHandler.load_train_test()
 
-        estimator = klass.pipeline(regressor)
-        grid = GridSearchCV(estimator, parameters, cv = 3)
+        grid = GridSearchCV(regressor, parameters, cv=3, verbose=1, n_jobs=-1)
         grid.fit(X, y)
-
-
         print 'Best parameters', grid.best_params_
-
 
         # Now look at the best estimator
         klass._check(grid.best_estimator_, name, X, y, X_test, y_test)
--- train/test_linear_models.py
+++ train/test_linear_models.py
@@ -3,6 +3,7 @@ from model.training import Trainer as ttr
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
 
+
 class TestDifferentModles(unittest.TestCase):
 
     # It's clear that the data is not linear
@@ -14,39 +15,32 @@ class TestDifferentModles(unittest.TestCase):
         from sklearn.linear_model import SGDRegressor
         ttr.check_model('SGD', SGDRegressor())
 
-
     @unittest.skip('')
     def test_lasso(self):
         from sklearn.linear_model import Lasso
         ttr.check_model('Lasso', Lasso())
 
-
+    @unittest.skip('')
     def test_ridge(self):
         from sklearn.linear_model import Ridge
-        make_pipeline([
+        ridge = make_pipeline([
             PolynomialFeatures(3),
-            ttr.check_model('Ridge', Ridge(alpha=0.1))
+            Ridge(alpha=0.1),
         ])
+        ttr.check_model('Ridge', ridge)
 
     @unittest.skip('')
     def test_elastic(self):
         from sklearn.linear_model import ElasticNet
         ttr.check_model('ElasticNet', ElasticNet(alpha=1))
 
-
-
-    @unittest.skip('')
     def test_svr(self):
         from sklearn.svm import SVR
         # TODO: Try to search in differnt kernel functions
         #       rbf, polynomial
-        ttr.check_model('SVR', SVR(kernel='poly', degree=4))
-
+        ttr.check_model('SVR', SVR(gamma=0.1))
 
     @unittest.skip('')
     def test_lars(self):
         from sklearn.linear_model import LassoLars
         ttr.check_model('LassoLars', LassoLars())
-
-
-
