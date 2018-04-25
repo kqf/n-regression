b99fc91a42802feb07995554ed9f39cfc35edb9b
Iohan Petrov <petrov.knu@gmail.com>
2018-04-25 17:04:58 +0100

    Add SVR scans for best parameter tunning

--- train/test_svm.py
+++ train/test_svm.py
@@ -2,14 +2,13 @@ import unittest
 from model.training import Trainer as ttr
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
 
 class TestSVM(unittest.TestCase):
 
-    def test_rbf(self):
-        from sklearn.svm import SVR
-        # TODO: Try grid search on different kernels
-        #
+    @unittest.skip('')
+    def test(self):
         kernels = [
             "poly",
             "sigmoid",
@@ -22,3 +21,17 @@ class TestSVM(unittest.TestCase):
                 SVR(kernel=kernel, C=25),
             )
             ttr.check_model('SVR ' + kernel, svr)
+
+    def test_scan(self):
+        parameters = {
+            "svr__kernel": ["rbf"],
+            # "kernel": ["rbf", "poly", "sigmoid"],
+            "svr__C": [1, 10, 25, 30],
+            "svr__gamma": ["auto", 2. / 16, 4. / 16, 1.]
+        }
+
+        svr = make_pipeline(
+            StandardScaler(),
+            SVR(),
+        )
+        ttr.search('SVR ', svr, parameters)
