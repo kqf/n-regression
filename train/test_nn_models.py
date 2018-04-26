70d5ec0e27f1fe1fcacd7a7a74779648ab12e293
Iohan Petrov <petrov.knu@gmail.com>
2018-04-26 08:35:03 +0100

    Scan different MLP configurationsg

--- train/test_nn_models.py
+++ train/test_nn_models.py
@@ -1,17 +1,29 @@
 import unittest
 from model.training import Trainer as ttr
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import PolynomialFeatures, Normalizer
-from sklearn.pipeline import make_pipeline, make_union
+from sklearn.pipeline import make_pipeline
 from sklearn.neural_network import MLPRegressor
-
+from sklearn.preprocessing import StandardScaler
 
 
 class TestCompositeModels(unittest.TestCase):
 
     # @unittest.skip('')
     def test_ridge(self):
-        ttr.check_model(
-            'MLPRegressor',
+        estimator = make_pipeline(
+            StandardScaler(),
             MLPRegressor(hidden_layer_sizes=(50, 15,))
         )
+
+        ttr.check_model('MLPRegressor', estimator)
+
+    def test_scan(self):
+        nsize = "mlpregressor__hidden_layer_sizes"
+        parameters = {
+            nsize: [(16,), (16, 8), (32, 1)]
+        }
+
+        svr = make_pipeline(
+            StandardScaler(),
+            MLPRegressor(),
+        )
+        ttr.search('SVR ', svr, parameters)
